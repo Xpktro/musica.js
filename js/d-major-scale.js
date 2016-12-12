0506113c1120fb1bf94a16eca2a7429990f35b08
Moisés Cachay Tello <mcachay@phantasia.pe>
2016-12-12 12:51:00 -0500

    Added major scale

---
+++ js/d-major-scale.js
@@ -0,0 +1,41 @@
+function majorScale() {
+  var        context = getAudioContext()
+  ,      currentTime = audioContext.currentTime
+  ,         duration = 0.5
+  , twelfthRootOfTwo = Math.pow(2, 1/12)
+  , currentFrequency = 220
+  ,      frequencies = [currentFrequency];
+
+  for (var i = 0; i < 12; i++) {
+    currentFrequency *= twelfthRootOfTwo;
+    frequencies.push(currentFrequency);
+  }
+
+  var           TONE = 2
+  ,         SEMITONE = 1
+  ,            scale = [TONE, TONE, SEMITONE, TONE, TONE, TONE, SEMITONE]
+  , currentFrequency = 0
+  , scaleFrequencies = [frequencies[0]];
+
+  scale.forEach(function (interval) {
+    currentFrequency += interval;
+    scaleFrequencies.push(frequencies[currentFrequency]);
+  })
+
+  scaleFrequencies.forEach(function(frequency, index) {
+    var sineosc = context.createOscillator()
+    , startTime = currentTime + (index * duration);
+    sineosc.type = 'sine';
+    sineosc.frequency.value = frequency;
+    sineosc.connect(context.destination);
+    sineosc.start(startTime);
+    sineosc.stop(startTime + duration);
+    sineosc.onended = function(){
+      log('Finished ' + frequency.toFixed(2) + 'Hz tone.');
+      if(index == scaleFrequencies.length - 1)
+        log('---');
+    };
+  });
+
+  log('Playing major scale...');
+}
